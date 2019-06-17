8c72b34151df8e36ba2ad915ee5d0c846faf7336
Habenzy <robertrstauss@gmail.com>
2019-06-17 10:01:25 -0400

    lingering in the deep forest now kills you, broadened acceptable input for room names, and implemented xyzzy teleport cheat

--- main.js
+++ main.js
@@ -40,8 +40,8 @@ const player = {
       player.inventory.push(item);
       player.currentRoom.inventory.pop(item)
       return `You pick up a ${item.name}`
-    } else { 
-      return "You can't take that" 
+    } else {
+      return "You can't take that"
     }
   },
 
@@ -154,14 +154,14 @@ const foothills = new Room('foothills', "You come out of the forest into rolling
 const hutYard = new Room('hutYard', "A small hut is nestled amongst the base of the mountains to the west.\nTo the East the forest looms. A road leads into cultivated fields to the North.\nRolling hills stretch as far as the eye can see to the south...", [], null, 'foothills', 'forestW', 'hut');
 const hut = new Room('hut', "The hut is a single room, cozy, and warm from the fire burning in the hearth.\nDespite the cleanliness of the cabin, and fire there are no signs of the occupants...", [], null, null, 'hutYard');
 hut.isLocked = true;
-const forestW = new Room('forestW', "The forest is dark and menacing, visibility is low\nand you hear animal noises all around. The trees seem to thin out to the North.\nIt would not be wise to linger...", [],'fieldW', 'glade', 'forest', 'hutYard' );
-const forest = new Room('forest', 'The trees are thinner here.\nYou can see cultivated fields through the trees to the north...', [], 'wheatField', 'deepForest', 'forestE','forestW' );
+const forestW = new Room('forestW', "The forest is dark and menacing, visibility is low\nand you hear animal noises all around. The trees seem to thin out to the North.\nIt would not be wise to linger...", [], 'fieldW', 'glade', 'forest', 'hutYard');
+const forest = new Room('forest', 'The trees are thinner here.\nYou can see cultivated fields through the trees to the north...', [], 'fieldC', 'deepForest', 'forestE', 'forestW');
 const deepForest = new Room('deepForest', "The forest is dark and menacing, visibility is low\nand you hear animal noises all around.\nIt would be most unwise to linger...", [], 'forest', 'field', 'deepForestE', 'glade');
 const deepForestE = new Room('deepForestE', "The forest is dark and menacing, visibility is low\nand you hear animal noises all around. It would not be wise to linger.\nThe trees seem to thin a bit to the East, and the South...", [], 'forestE', 'clearing', 'riverS', 'deepForest');
 const forestE = new Room('forestE', "The trees are thinner here.\nYou can see cultivated fields through the trees to the north.\nYou here the sound of running water to the East...", [], 'fieldE', 'deepForestE', 'river', 'forest');
 const river = new Room('river', "You stand on the Western bank of a river flowing swiftly from South to North\nTo the West the forest looms...", [], 'riverN', 'riverS', null, 'forestE');
 const riverS = new Room('riverS', "You stand on the Western bank of a river flowing swiftly from South to North\nTo the West the forest looms.\nThe southern cliffs can be seen above the treeline to the South...", [], 'river', 'caveEnterance', null, 'deepForestE');
-const riverN = new Room('riverN', "You stand on the Western bank of a river flowing swiftly from South to North\nTo the West cultivated fields spread into the distance.\nTo the North the river dissapears beneath the city walls...", [], null, 'river',null, 'fieldE');
+const riverN = new Room('riverN', "You stand on the Western bank of a river flowing swiftly from South to North\nTo the West cultivated fields spread into the distance.\nTo the North the river dissapears beneath the city walls...", [], null, 'river', null, 'fieldE');
 const fieldC = new Room('fieldC', "A road leads through fields of golden wheat.\nTo the north a set of massive gates are set into a gigantic wall.\nRoofs, and towers can be seen beyond the walls.\nMore fields stretch to the East and West. To the south the forest looms.\nEverything is earily silent...", [], null, 'forest', 'fieldE', 'fieldW');
 const fieldE = new Room('fieldE', "You stand amidst a field of ripe wheat. A river flows along the Eastern side of the field.\nThe city walls tower over the Northern end of the field.\nTo the South the forest stretches into the distance...", [], null, 'forestE', 'riverN', 'fieldC');
 const fieldW = new Room('fieldW', "You stand amidst a field of ripe wheat. An impassable mountain range shades\nthe Western side of the field.\nThe city walls tower over the Northern end of the field.\nTo the South the forest stretches into the distance...", [], null, 'forestW', 'fieldC', null);
@@ -175,36 +175,48 @@ const caveEnterance = new Room('caveEnterance', "The river rushes through a dark
 const obObjs = {
   'rocks': rock,
   'rock': rock,
-  'sticks':stick,
+  'sticks': stick,
   'stick': stick,
   'shiney': key,
-  'key' : key
+  'key': key
 }
 
 //rooms table
 const obRooms = {
   'canyon': canyon,
   'field': field,
-  'clearing' : clearing,
+  'clearing': clearing,
   'shack': shack,
   'deepForestW': deepForestW,
-  'glade' : glade,
-  'foothills' : foothills,
-  'hutYard' : hutYard,
-  'hut' : hut,
-  'forestW' : forestW,
-  'forest' : forest,
-  'deepForest' : deepForest,
-  'deepForestE' : deepForestE,
-  'forestE' : forestE,
-  'river' : river,
-  'riverS' : riverS,
-  'riverN' : riverN,
-  'fieldC' : fieldC,
-  'fieldE' : fieldE,
-  'fieldW' : fieldW,
+  'deepforestw': deepForestW,
+  'glade': glade,
+  'foothills': foothills,
+  'hutYard': hutYard,
+  'hutyard': hutYard,
+  'hut': hut,
+  'forestW': forestW,
+  'forestw' : forestW,
+  'forest': forest,
+  'deepForest': deepForest,
+  'deepforest' : deepForest,
+  'deepForestE': deepForestE,
+  'deepforeste' : deepForestE,
+  'forestE': forestE,
+  'foreste': forestE,
+  'river': river,
+  'riverS': riverS,
+  'rivers': riverS,
+  'riverN': riverN,
+  'rivern': riverN,
+  'fieldC': fieldC,
+  'fieldc': fieldC,
+  'fieldE': fieldE,
+  'fielde': fieldE,
+  'fieldW': fieldW,
+  'fieldw': fieldW,
   'caveEnterance': caveEnterance,
-  'mountains' : mountains
+  'caveenterance': caveEnterance,
+  'mountains': mountains
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -260,6 +272,11 @@ async function play() {
       player.inventory.forEach(obj => console.log(obj.name));
       play();
     }
+  }
+
+  else if (thisAction === 'linger' && player.currentRoom.name.includes('Forest')) {
+    console.log("As you linger in the forest you hear movement all around you.\nFirst one pair of glowing red eyes appears through the undergrowth,\nthen another, then a hundred more.  All at once the beasts pounce on you\ntearing you to pieces in an explosion of gore.\nYou have died.");
+    process.exit();
   }
 
   //move
@@ -329,6 +346,15 @@ async function play() {
     }
   }
 
+  else if (thisAction === 'xyzzy' && obRooms[focus]) {
+    if (obRooms[focus].isLocked) {
+      obRooms[focus].unlock()
+    }
+    player.changeRoom(obRooms[focus])
+    console.log(player.currentRoom.enterRoom())
+    play();
+  }
+
   //examine objects
   else if (commands.examine.includes(thisAction) && player.currentRoom.inventory.includes(obObjs[focus])) {
     let item = focus;
@@ -373,7 +399,7 @@ async function play() {
   }
 
   else if (commands.unlock.includes(thisAction)) {
-    if(player.inventory.includes(key)){
+    if (player.inventory.includes(key)) {
       key.action();
       play()
     }
@@ -392,6 +418,6 @@ async function play() {
 startGame();
 
 //To Do:
-//  NPCs or the myst approach? Which should I do? NPCs would need another chunk of behavioral code & object template...
+//  NPCs or the myst approach? Which should I do? NPCs would need another chunk of behavioral code & class template...
 // create an actual narative/world
 
